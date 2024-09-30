a360b9e8bfb782acc7157b0dae1db1253f143104
Alexander Dines <160077924+dines-rl@users.noreply.github.com>
2024-09-30 15:54:19 -0700

    cp dines

--- test/treesitter.test.ts
+++ test/treesitter.test.ts
@@ -2,5 +2,5 @@ import { describe, expect, test } from "vitest";
 import { ASTForFile } from "../dist/AstforFile.js";
 describe("TreeSitter", () => {
   test("parses a file", () =>
-    expect(ASTForFile("./fixtures/testJSTFile.ts")).toMatchSnapshot());
+    expect(ASTForFile("test/fixtures/testJSTFile.ts")).toMatchSnapshot());
 });
